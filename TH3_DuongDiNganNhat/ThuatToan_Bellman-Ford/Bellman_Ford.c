e607c7a235c66cd8c5ff8d38503388f9b25ed6bf
Doan <duonghongdoan.hp2020@gmail.com>
2022-09-28 08:33:41 +0700

    Upload BellmanFord Algorithm

--- TH3_DuongDiNganNhat/ThuatToan_Bellman-Ford/Bellman_Ford.c
+++ TH3_DuongDiNganNhat/ThuatToan_Bellman-Ford/Bellman_Ford.c
@@ -25,93 +25,93 @@ void add_edge(Graph *pG, int u, int v, int w) {
 
 int pi[max_n], p[max_n];
 int negative_cycle = 0;
-//void bellman_Ford(Graph *pG,int s) {
-//	int u, v, w,i, k;
-//	for (u=1; u<=pG->n; u++) {
-//		pi[u] = oo;
-//		p[u] = 0;
-//	}
-//	
-//	pi[s] = 0;
-//	p[s] = -1;
-//	
-//	
-//	for (i=1; i<pG->n; i++) {
-//		for (k=0; k<pG->m; k++) {
-//			u = pG->edges[k].u;
-//			v = pG->edges[k].v;
-//			w = pG->edges[k].w;
-//			if (pi[u] == oo)
-//				continue;
-//			if (pi[u] + w < pi[v]) {
-//				pi[v] = pi[u] + w;
-//				p[v] = u;
-//			}
-//		}
-//	}
-//	
-//	//Kiem tra chu trinh am
-//	for (k=0; k<pG->m; k++) {
-//		u = pG->edges[k].u;
-//		v = pG->edges[k].v;
-//		w = pG->edges[k].w;
-//		int t = pi[u] + w;
-//		if ((t<=oo && t>=900000)) {
-//			t = oo;
-//			if (t < pi[v]) {
-//				negative_cycle = 1;
-//				break;
-//			}
-//		}
-//		else if(t<oo) {
-//			if (t < pi[v]) {
-//				negative_cycle = 1;
-//				break;
-//			}
-//		}	
-//	}
-//}
-
-int BellmanFord(Graph *pG, int s) {
-	int u, v, w, it, k;
-	for (u = 1; u <= pG->n; u++) {
+void BellmanFord(Graph *pG,int s) {
+	int u, v, w,i, k;
+	for (u=1; u<=pG->n; u++) {
 		pi[u] = oo;
+		p[u] = 0;
 	}
+	
 	pi[s] = 0;
-	p[s] = -1; //tru?c d?nh s không có d?nh nào c?
-
-	// l?p n-1 l?n
-	for (it = 1; it < pG->n; it++) {
-		// Duy?t qua các cung và c?p nh?t (n?u tho?)
-		for (k = 0; k < pG->m; k++) {
+	p[s] = -1;
+	
+	
+	for (i=1; i<pG->n; i++) {
+		for (k=0; k<pG->m; k++) {
 			u = pG->edges[k].u;
 			v = pG->edges[k].v;
 			w = pG->edges[k].w;
-			
-			if (pi[u] == oo)    //chua có du?ng di t? s -> u, b? qua cung này
-			    continue;
-			    
+			if (pi[u] == oo)
+				continue;
 			if (pi[u] + w < pi[v]) {
 				pi[v] = pi[u] + w;
 				p[v] = u;
 			}
 		}
 	}
-	//Làm thêm 1 l?n n?a d? ki?m tra chu trình âm (n?u c?n thi?t)
-	for (k = 0; k < pG->m; k++) {
+	
+	//Kiem tra chu trinh am
+	for (k=0; k<pG->m; k++) {
 		u = pG->edges[k].u;
 		v = pG->edges[k].v;
 		w = pG->edges[k].w;
-		
-		if (pi[u] == oo)    //chua có du?ng di t? s -> u, b? qua cung này
-		    continue;
-		    
-		if (pi[u] + w < pi[v]) {
-			return 1;
+		int t = pi[u] + w;
+		if ((t<=oo && t>=900000)) {
+			t = oo;
+			if (t < pi[v]) {
+				negative_cycle = 1;
+				break;
+			}
 		}
+		else if(t<oo) {
+			if (t < pi[v]) {
+				negative_cycle = 1;
+				break;
+			}
+		}	
 	}
-	return 0;
 }
+
+//int BellmanFord(Graph *pG, int s) {
+//	int u, v, w, it, k;
+//	for (u = 1; u <= pG->n; u++) {
+//		pi[u] = oo;
+//	}
+//	pi[s] = 0;
+//	p[s] = -1; //truoc dinh s khong co dinh nao
+//
+//	// lap n-1 lan
+//	for (it = 1; it < pG->n; it++) {
+//		// Duyet qua các cung và cap nhat (neu thoa)
+//		for (k = 0; k < pG->m; k++) {
+//			u = pG->edges[k].u;
+//			v = pG->edges[k].v;
+//			w = pG->edges[k].w;
+//			
+//			if (pi[u] == oo)    //chua có duong di tu s -> u, bo qua cung này
+//			    continue;
+//			    
+//			if (pi[u] + w < pi[v]) {
+//				pi[v] = pi[u] + w;
+//				p[v] = u;
+//			}
+//		}
+//	}
+//	//Làm thêm 1 lan nua de kiem tra chu trình âm (neu can thiet)
+//	for (k = 0; k < pG->m; k++) {
+//		u = pG->edges[k].u;
+//		v = pG->edges[k].v;
+//		w = pG->edges[k].w;
+//		
+//		if (pi[u] == oo)    //chua có duong di tu s -> u, bo qua cung này
+//		    continue;
+//		    
+//		if (pi[u] + w < pi[v]) {
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
 
 int main () {
 	Graph G;
@@ -125,20 +125,21 @@ int main () {
 		add_edge(&G, u, v, w);
 	}
 	
-	scanf ("%d%d", &s, &t);
-//	bellman_Ford(&G, s);
-/*kiem tra chu trinh am*/
-//	if (negative_cycle)
-//		printf ("YES");
-//	else 
-//		printf ("NO");
-		
+//	scanf ("%d%d", &s, &t);
+	BellmanFord(&G, 1);
+/*kiem tra chu trinh am*/	
 //	if (BellmanFord(&G, s)==1)
-//		printf ("YES");
+//		printf ("YES\n");
 //	else 
-//		printf ("NO");
-
+//		printf ("NO\n");
+	if (negative_cycle==1)
+		printf ("YES\n");
+	else 
+		printf ("NO\n");
+/*in ra chieu dai duong di ngan nhat va dinh truoc no*/	
+	for (u=1; u<=n; u++) {
+		printf ("pi[%d] = %d, p[%d] = %d\n", u, pi[u], u, p[u]);
+	}
 	
-	int path[]
 	return 0;
 }
